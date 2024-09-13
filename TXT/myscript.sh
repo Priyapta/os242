e147297e69857a2f45b59408e56d366e32b00393
Priyapta Naufal <priaptanaufal@gmail.com>
2024-09-13 16:57:03 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -19,7 +19,7 @@
 REC2="549E97AF212092DF"
 # ####################### ####### #### ####
 # REC1: public key
-REC1="A1FA23615D7CAF9C"
+REC1="63FB12B215403B20"
 # WEEKURL="http://localhost:4000/WEEK/WEEK.txt"
 WEEKURL="https://os.vlsm.org/WEEK/WEEK.txt"
 FILES="my*.asc my*.txt my*.sh"
